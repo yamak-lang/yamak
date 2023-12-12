a2c7e659bb5ac5467c19bfdbacf719d7f0cdb030
Naman Dixit <code@namandixit.net>
2023-12-12 20:47:14 +0530

    Dummy commit to test Github Workflows

--- src/main/java/net/namandixit/java/yamak/Yamak.java
+++ src/main/java/net/namandixit/java/yamak/Yamak.java
@@ -4,6 +4,6 @@ public class Yamak {
 
     public static void main(String[] args) {
         Compiler compiler = new Compiler();
-        compiler.runString("", "23.34 hello bye func lambda 0x1.400p3 0x10.1p0 123.456p-67 #{Commenting #{nested {{{ #} }}} #} #byeeeee 420");
+        compiler.runString("", "true false 23.34 hello bye func lambda 0x1.400p3 0x10.1p0 123.456p-67 #{Commenting #{nested {{{ #} }}} #} #byeeeee 420");
     }
 }
